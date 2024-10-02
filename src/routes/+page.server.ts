17cba3793765928963599bcfb11558a987b1cd8d
Subrath <subrathitachi6@gmail.com>
2024-10-02 21:05:14 +0530

    remove node mailer

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -14,28 +14,9 @@ export const actions = {
 			if (!name || !email || !subject || !content) {
 				throw error(400, { message: 'all fields are mandatotry' });
 			}
-			console.log(formData);
-			const transporter = nodemailer.createTransport({
-				host: 'smtp.gmail.com',
-				port: 587,
-				auth: {
-					user: 'subrathitachi6@gmail.com',
-					pass: 'kyfj oswm dtsx odkw'
-				}
-			});
-			const info = await transporter.sendMail({
-				from: 'subrathitachi6@gmail.com', // sender address
-				to: 'subraths@proton.me', // list of receivers
-				subject: 'Message from portfolio', // Subject line
-				text: `name: ${name}\n email: ${email}\n subject:${subject}\n ${content}` // plain text body
-			});
-
-			console.log('Message sent: %s', info.messageId);
 			return {
 				success: true,
-				data: {
-					name: formData.get('name')
-				}
+				name
 			};
 		} catch (error) {
 			console.log(error);
