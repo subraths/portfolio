7a38dcb3fba6a4e0d86d5900fc1f2418d43379f6
Subrath <subrathitachi6@gmail.com>
2025-10-23 12:23:23 +0530

    remove cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,10 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		alias: {
 			$comp: 'src/components'
-		},
-		adapter: adapter({
-			routes: {
-				include: ['/*']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
+		}
 	}
 };
 
