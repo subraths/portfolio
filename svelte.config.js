30aa5c77bb11786a10f18e981ec58d8cc60dbc7b
Subrath <subrathitachi6@gmail.com>
2025-10-23 13:06:57 +0530

    add node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
+
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		alias: {
 			$comp: 'src/components'
-		}
+		},
+		adapter: adapter()
 	}
 };
 
